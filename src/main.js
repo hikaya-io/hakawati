062b00dda562d5b9d61643094225af3f1f1e2425
Michael Bukachi <michaelbukachi@gmail.com>
2020-03-19 19:50:59 +0300

    Adjust custom theme (#2)
    * UI-57: #comment add custom theme index.css file
    
    * refactor: Update default theme used
    
    Co-authored-by: andrewtpham <andrew.tc.pham@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'src/theme/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
