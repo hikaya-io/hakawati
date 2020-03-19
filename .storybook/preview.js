4101a324fafa45b252756777acaac6e553b3f21f
Michael Bukachi <michaelbukachi@gmail.com>
2020-03-19 21:06:49 +0300

    build: Setup scss preprocessing

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/vue';
 
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import '../src/theme/index.css'
+import '@/styles/element-variables.scss'
 
 // Install Vue plugins.
 Vue.use(ElementUI);
