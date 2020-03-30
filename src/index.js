d8ae950698c9838227430eb5fb4dad03d9f4d1a8
Michael Bukachi <michaelbukachi@gmail.com>
2020-03-30 19:02:56 +0300

    build: Add command for building library (#9)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import BasicButton from './components/button/BasicButton.vue'
+// Import other components
+
+export default () => {
+  return {
+    BasicButton
+    // Add other components here
+  }
+}
