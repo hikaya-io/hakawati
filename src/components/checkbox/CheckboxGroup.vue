aa1ad71002a7f3e440cb0eab21ba00142a731324
Andrew <andrew.tc.pham@gmail.com>
2020-04-09 12:40:36 +0200

    Update issue templates (#23)
    * UI-55 #comment added checkbox components
    
    * update issue templates
    
    * update issue templates

---
+++ src/components/checkbox/CheckboxGroup.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-checkbox-group @click="$emit('click')" :type="type" v-model="checkList">
+    <slot></slot>
+  </el-checkbox-group>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxGroup',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      checkList: ['Option A', 'Selected and disabled']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
