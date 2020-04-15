2ef28e4c675a6da92c50e6957821130612d64abf
Erin Thomas <erin.c.tho@gmail.com>
2020-04-15 17:26:29 -0400

    Add select component

---
+++ src/components/select/GroupedSelect.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-select
+    v-model="value"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    @change="$emit('change', $event)"
+  >
+    <el-option-group v-for="group in options" :key="group.label" :label="group.label">
+      <el-option
+        v-for="item in group.options"
+        :key="item.value"
+        :label="item.label"
+        :disabled="item.disabled"
+        :value="item.value"
+      />
+    </el-option-group>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: "GroupedSelect",
+  data() {
+    return {
+      value: ""
+    };
+  },
+  props: {
+    options: {
+      type: Array,
+      default: []
+    },
+    placeholder: {
+      type: String,
+      default: "Select an option"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.el-select {
+  font-family: "Lato", sans-serif;
+  font-size: 16px;
+}
+
+li.el-select-group__title {
+    font-family: "Lato", sans-serif;
+    font-size: 14px;
+    color: #848c9c;
+    margin: 0px 12px;
+    padding-left: 8px;
+    font-weight: 600;
+    /* border-bottom: 1px solid #5a5f6955; */
+}
+</style>
---
+++ src/components/select/MultiSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-select
+    v-model="value"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    :collapse-tags="collapseTags"
+    multiple
+    @change="$emit('change', $event)"
+  >
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :disabled="item.disabled"
+      :value="item.value"
+    />
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: "MultiSelect",
+  data() {
+    return {
+      value: []
+    };
+  },
+  props: {
+    options: {
+      type: Array,
+      default: []
+    },
+    placeholder: {
+      type: String,
+      default: "Select an option"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    collapseTags: {
+        type: Boolean,
+        default: false
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.el-select {
+  font-family: "Lato", sans-serif;
+  font-size: 16px;
+}
+</style>
