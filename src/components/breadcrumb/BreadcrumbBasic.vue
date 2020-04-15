f58a47362ad0b6e1c221a1841471e60e08a1eb9a
Erin Thomas <aron.c.thomas@gmail.com>
2020-04-15 17:28:40 -0400

    Add breadcrumb component (#41)
    * Add breadcrumb component
    
    * add font size

---
+++ src/components/breadcrumb/BreadcrumbBasic.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-breadcrumb :separator="separator">
+        <el-breadcrumb-item 
+            v-for="(item, index) in items" 
+            :key="index" 
+            :to="item.to"
+            :replace="item.replace">
+            {{item.label}}
+        </el-breadcrumb-item>
+    </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'BreadcrumbBasic',
+  props: {
+    items: {
+      type: Array,
+      default: []
+    },
+    separator: {
+        type: String,
+        default: '/'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.el-breadcrumb {
+font-family: 'Lato', sans-serif;
+font-size: 16px;
+}
+</style>
