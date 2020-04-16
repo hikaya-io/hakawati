bfc7411ce3b150eec25088948104a8b423dccde4
Erin Thomas <aron.c.thomas@gmail.com>
2020-04-16 15:41:15 -0400

    Add select component (#42)
    Co-authored-by: Andrew <andrew.tc.pham@gmail.com>

---
+++ src/components/select/BasicSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-select
+    v-model="value"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    @change="$emit('change', $event)"
+  >
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :disabled="item.disabled"
+      :value="item.value"
+    />
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: "BasicSelect",
+  data() {
+    return {
+      value: ''
+    };
+  },
+  props: {
+    options: {
+      type: Array,
+      default: []
+    },
+    placeholder: {
+      type: String,
+      default: "Select an option"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.el-select {
+  font-family: "Lato", sans-serif;
+  font-size: 16px;
+}
+</style>
