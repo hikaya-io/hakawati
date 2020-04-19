117d7a49725893bff175ec245f580feefc13e853
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-04-19 04:19:43 +0200

    Table with sorting

--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -2,7 +2,9 @@ import BasicTable from '../components/table/BasicTable.vue'
 import TableGroupingHead from '../components/table/TableGroupingHead.vue'
 import TableMultipleSelect from '../components/table/TableMultipleSelect.vue'
 import TableWithBorder from '../components/table/TableWithBorder.vue'
+import TableWithFilter from '../components/table/TableWithFilter.vue'
 import TableWithFixedHeader from '../components/table/TableWithFixedHeader.vue'
+import TableWithSorting from '../components/table/TableWithSorting.vue'
 import TableWithStatus from '../components/table/TableWithStatus.vue'
 
 // This is required for each story
@@ -13,20 +15,29 @@ export const basicTable = () => ({
   template: '<basic-table />'
 })
 
-export const tableWithStatus = () => ({
-  components: { TableWithStatus },
-  template: '<table-with-status />'
-})
 
 export const tableWithBorder = () => ({
   components: { TableWithBorder },
   template: '<table-with-border />'
 })
-
+export const tableWithFilter = () => ({
+  components: { TableWithFilter },
+  template: '<table-with-filter />'
+})
 // Scroll through rows to see the effect
 export const tableWithFixedHeader = () => ({
   components: { TableWithFixedHeader },
   template: '<table-with-fixed-header />'
+})
+
+export const tableWithSorting = () => ({
+  components: { TableWithSorting },
+  template: '<table-with-sorting />'
+})
+
+export const tableWithStatus = () => ({
+  components: { TableWithStatus },
+  template: '<table-with-status />'
 })
 
 export const tableGroupingHead = () => ({
