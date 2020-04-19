c7e52055877f21df6226b5b8bb3e23165901f6e8
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-04-19 04:02:10 +0200

    Table with fixed header

--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -1,5 +1,6 @@
 import BasicTable from '../components/table/BasicTable.vue'
 import TableWithBorder from '../components/table/TableWithBorder.vue'
+import TableWithFixedHeader from '../components/table/TableWithFixedHeader.vue'
 import TableWithStatus from '../components/table/TableWithStatus.vue'
 
 // This is required for each story
@@ -18,4 +19,10 @@ export const tableWithStatus = () => ({
 export const tableWithBorder = () => ({
   components: { TableWithBorder },
   template: '<table-with-border />'
+})
+
+// Scroll through rows to see the effect
+export const tableWithFixedHeader = () => ({
+  components: { TableWithFixedHeader },
+  template: '<table-with-fixed-header />'
 })
