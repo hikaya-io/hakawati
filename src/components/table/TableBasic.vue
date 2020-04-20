400d515b462a530d73a882620e7d69ba38ef9579
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-04-20 21:01:05 +0200

    Props for basic table,naming change

---
+++ src/components/table/TableBasic.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-table
+      :data="dataTable"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="Date"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="Name"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="Address"
+      >
+      </el-table-column>
+    </el-table>
+</template>
+
+<script>
+export default {
+  mounted(){
+    // Showing table data from props
+    console.log(this.dataTable)
+  },
+  props: ["dataTable"], 
+};
+</script>
--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -1,69 +1,107 @@
-import BasicTable from '../components/table/BasicTable.vue'
-import TableGroupingHead from '../components/table/TableGroupingHead.vue'
-import TableMultipleSelect from '../components/table/TableMultipleSelect.vue'
+import TableBasic from '../components/table/TableBasic.vue'
 import TableWithBorder from '../components/table/TableWithBorder.vue'
 import TableWithExpandableRow from '../components/table/TableWithExpandableRow.vue'
 import TableWithFilter from '../components/table/TableWithFilter.vue'
 import TableWithFixedHeader from '../components/table/TableWithFixedHeader.vue'
+import TableWithGroupingHead from '../components/table/TableWithGroupingHead.vue'
+import TableWithMultipleSelect from '../components/table/TableWithMultipleSelect.vue'
 import TableWithRowSpan from '../components/table/TableWithRowSpan.vue'
 import TableWithSorting from '../components/table/TableWithSorting.vue'
 import TableWithStatus from '../components/table/TableWithStatus.vue'
 import TableWithSummaryRow from '../components/table/TableWithSummaryRow.vue'
 
 // This is required for each story
-export default { title: 'Table' }
+export default {
+  title: 'Table'
+}
+ 
+export const tableBasic = () => ({
+  components: {
+    TableBasic
+  },
+  data() {
+    return {
+      dataTable: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
+  },
+  template: '<table-basic :dataTable="dataTable" />',
 
-export const basicTable = () => ({
-  components: { BasicTable },
-  template: '<basic-table />'
 })
 
 export const tableWithBorder = () => ({
-  components: { TableWithBorder },
-  template: '<table-with-border />'
+  components: {
+    TableWithBorder
+  }, 
+  template: '<table-with-border   />'
 })
 
 export const tableWithExpandableRow = () => ({
-  components: { TableWithExpandableRow },
+  components: {
+    TableWithExpandableRow
+  },
   template: '<table-with-expandable-row />'
 })
 
 export const tableWithFilter = () => ({
-  components: { TableWithFilter },
+  components: {
+    TableWithFilter
+  },
   template: '<table-with-filter />'
 })
 // Scroll through rows to see the effect
 export const tableWithFixedHeader = () => ({
-  components: { TableWithFixedHeader },
+  components: {
+    TableWithFixedHeader
+  },
   template: '<table-with-fixed-header />'
 })
 
 export const tableWithRowSpan = () => ({
-  components: { TableWithRowSpan },
+  components: {
+    TableWithRowSpan
+  },
   template: '<table-with-row-span />'
 })
 
 export const tableWithSorting = () => ({
-  components: { TableWithSorting },
+  components: {
+    TableWithSorting
+  },
   template: '<table-with-sorting />'
 })
 
 export const tableWithStatus = () => ({
-  components: { TableWithStatus },
+  components: {
+    TableWithStatus
+  },
   template: '<table-with-status />'
 })
 
 export const tableWithSummaryRow = () => ({
-  components: { TableWithSummaryRow },
+  components: {
+    TableWithSummaryRow
+  },
   template: '<table-with-summary-row />'
 })
 
-export const tableGroupingHead = () => ({
-  components: { TableGroupingHead },
-  template: '<table-grouping-head />'
+export const tableWithGroupingHead = () => ({
+  components: {
+    TableWithGroupingHead
+  },
+  template: '<table-with-grouping-head />'
 })
 
-export const tableMultipleSelect = () => ({
-  components: { TableMultipleSelect },
-  template: '<table-multiple-select />'
+export const tableWithMultipleSelect = () => ({
+  components: {
+    TableWithMultipleSelect
+  },
+  template: '<table-with-multiple-select />'
 })
