eb0b3a188ca57f3ba4b0ac6d2e7e958899e042e9
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-04-21 22:53:07 +0300

    Ui 16: date picker (#56)
    * feat: Added date range picker
    
    see #16
    
    * feat: added basic date picker
    
    added a basic date picker to the library, with the following possible props
    - dateformat
    - size
    - type
    
    see #16
    
    * added sample date format props
    
    * fixed date format typo
    
    Co-authored-by: Andrew <andrew.tc.pham@gmail.com>

---
+++ src/components/datepicker/DateRangePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-date-picker
+    v-model="daterange"
+    :type="type"
+    :format="format"
+    range-separator="To"
+    start-placeholder="Start date"
+    end-placeholder="End date"
+  ></el-date-picker>
+</template>
+
+<script>
+export default {
+  name: "DateRange",
+  props: {
+    type: {
+      type: String,
+      default: "daterange"
+    },
+    format: {
+      type: String,
+      default: "yyyy-MM-dd"
+    },
+    size: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      daterange: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
