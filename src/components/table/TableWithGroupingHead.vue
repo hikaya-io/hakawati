1057cbe4422ab2c7a7ffe9e22b1bfa6c9965ab08
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-04-25 21:47:58 +0200

    Table with Row Span

--- src/components/table/TableWithGroupingHead.vue
+++ src/components/table/TableWithGroupingHead.vue
@@ -3,43 +3,55 @@
     :data="tableData"
     style="width: 100%"
   >
-    <el-table-column
-      prop="date"
-      label="Date"
-      width="150"
-    >
-    </el-table-column>
-    <el-table-column label="Delivery Info">
+    <el-table-column label="Delivsssery sdfInfo">
       <el-table-column
-        prop="name"
-        label="Name"
-        width="120"
+        prop="date"
+        label="Date"
+        width="150"
       >
       </el-table-column>
-      <el-table-column label="Address Info">
+      <el-table-column label="Delivery Info">
         <el-table-column
-          prop="state"
-          label="State"
+          prop="name"
+          label="Name"
           width="120"
         >
         </el-table-column>
-        <el-table-column
-          prop="city"
-          label="City"
-          width="120"
-        >
+        <el-table-column label="Address Info">
+          <el-table-column
+            prop="state"
+            label="State"
+            width="120"
+          >
+
+          </el-table-column>
+          <el-table-column
+            prop="city"
+            label="City"
+            width="120"
+          > </el-table-column>
         </el-table-column>
+        <el-table-column label="AOther">
         <el-table-column
           prop="address"
           label="Address"
           width="300"
         >
         </el-table-column>
+
         <el-table-column
-          prop="zip"
-          label="Zip"
-          width="120"
+          prop="address"
+          label="Address"
+          width="300"
         >
+        </el-table-column>
+</el-table-column>
+        <el-table-column
+          prop="address"
+          label="Address"
+          width="300"
+        >
+
         </el-table-column>
       </el-table-column>
     </el-table-column>
