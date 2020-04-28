1e0ac1ba03e811b7220d92287ad349185c59af7c
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-04-28 16:36:10 +0300

    UI 53: Steps (#60)
    * added basic steps component
    
    - recieves steps array as prop ( not required )
    - will display a default of three steps if not passed
    
    see #53
    
    * added description to the steps component
    
    - Step description can be passed in the step object ( not required )
    
    see #53
    
    * added simple step bar to basic steps
    
    - Simple step bar can be toggled by passing isSimple as true
    - isSimple flag is not required
    
    see #53
    
    * added Vertical step bar as a prop
    
    - the step bar can be changed to be verical by using the direction property as vertical
    - the direction property is not required and will default to horizontal
    
    see #53
    
    * added steps to the list of components
    
    * added button to trigger change in steps
    
    * style: Change the `finish-status` and styling
    
    - `finish-status` is set to `success` to see the check mark after a step is completed
    - CSS is added to change the color of the completed steps from green to Hikaya blue
    
    Co-authored-by: Andrew <andrew.tc.pham@gmail.com>
    Co-authored-by: Ninette Adhikari <ninette.adhikari@gmail.com>

---
+++ src/components/steps/BasicStepsWithDescription.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-steps :active="active" finish-status="success" :direction="direction">
+    <el-step
+      v-for="(step, i) in steps"
+      :key="i"
+      :title="step.title"
+      :description="step.description"
+      :space="200"
+    ></el-step>
+  </el-steps>
+</template>
+<script>
+export default {
+  name: 'BasicSteps',
+  props: {
+    active: {
+      type: Number,
+      default: 0
+    },
+    steps: {
+      type: Array,
+      default: () => [
+        {
+          title: 'Step 1',
+          description: 'Description 1'
+        },
+        {
+          title: 'Step 2',
+          description: 'Description 2'
+        },
+        {
+          title: 'Step 3',
+          description: 'Description 1'
+        }
+      ]
+    },
+    direction: {
+      type: String,
+      default: 'horizontal'
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.el-step__head.is-success {
+  color: #25ced1;
+  border-color: #25ced1;
+}
+.el-step__title.is-success {
+  color: #25ced1;
+}
+.el-step__description.is-success {
+  color: #25ced1;
+}
+</style>
---
+++ src/stories/Steps.stories.js
@@ -0,0 +1,97 @@
+import BasicSteps from '../components/steps/BasicSteps.vue';
+import BasicStepsWithDescription from '../components/steps/BasicStepsWithDescription.vue';
+
+export default { title: 'Steps' }
+
+export const basicSteps = () => ({
+    components: { BasicSteps },
+    data() {
+        return {
+            active: 0,
+            steps: [
+                'Wake up',
+                'Drink cofee',
+                'Be awesome',
+                'Sleep'
+            ]
+        }
+    },
+    methods: {
+        next() {
+            if (this.active++ >= this.steps.length) this.active = 0;
+        }
+    },
+    template: `
+    <div>
+    Default
+    <basic-steps :active="active"/>
+    <br>
+    Passing array of steps
+    <basic-steps :steps="steps" :active="active" />
+    <br>
+    Change direction to horizontal
+    <basic-steps :steps="steps" direction="vertical" :active="active" />
+    <br>
+    Simple Step bar
+    <basic-steps :steps="steps" :isSimple="true" orientation="vertical" :active="active" />
+    <br>
+    <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
+    </div>`
+})
+
+export const withDescription = () => ({
+    components: { BasicStepsWithDescription },
+    methods: {
+        next() {
+            if (this.active++ >= this.steps.length) this.active = 0;
+        }
+    },
+    data() {
+        return {
+            active: 0,
+            steps: [
+                {
+                    title: 'Wakeup'
+                },
+                {
+                    title: 'Drink Coffee'
+                },
+                {
+                    title: 'Be awesome',
+
+                },
+                {
+                    title: 'Sleep',
+                }
+            ],
+            stepsWithDesc: [
+                {
+                    title: 'Alarm rings',
+                    description: 'Time to wakeup'
+                },
+                {
+                    title: 'Coffee has boiled',
+                    description: 'Drink Coffee'
+                },
+                {
+                    title: 'Be awesome',
+                    description: 'Write some code'
+                },
+                {
+                    title: 'Sleep',
+                    description: 'Get some rest'
+                }
+            ]
+        }
+    },
+    template: `
+    <div>Vertical step bar
+    Default
+    <basic-steps-with-description :active="active"/>
+    <br>
+    Passing array of steps
+    <basic-steps-with-description :steps="stepsWithDesc"  :active="active"/>
+    <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
+    </div>
+    `
+})
