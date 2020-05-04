69ca65618b9df8e5842ca002d058f7faf1c79147
Tanvir Rahman <rahmantanvir@gmail.com>
2020-05-04 21:54:20 +0600

    introduced basic form

--- src/stories/Form.stories.js
+++ src/stories/Form.stories.js
@@ -1,10 +1,46 @@
-import BasicForm from '../components/form/BasicForm.vue'
+import HkForm from '../components/form/Form.vue'
 
 // This is required for each story
 export default { title: 'Form' }
 
-// Customize components here
-export const basicForm = () => ({
-  components: { BasicForm },
-  template: '<basic-form />'
+export const hkForm = () => ({
+  components: { HkForm },
+  data () {
+    return {
+      form: {
+        input: ''
+      }
+    }
+  },
+  template: `
+    <div>
+      <hk-form :model="form">
+        <el-form-item>
+          <el-input
+            placeholder="Sample Input"
+            v-model="form.input"
+          />
+        </el-form-item>
+      </hk-form>
+      <hk-form
+        label-position="top"
+        label-width="100px"
+        :model="form"
+      >
+        <el-form-item label="Sample Input with label">
+          <el-input v-model="form.input" />
+        </el-form-item>
+      </hk-form>
+      <hk-form
+        label-position="top"
+        label-width="100px"
+        :model="form"
+        :disabled="true"
+      >
+        <el-form-item label="Sample Input with label">
+          <el-input v-model="form.input" />
+        </el-form-item>
+      </hk-form>
+    </div>
+  `
 })
