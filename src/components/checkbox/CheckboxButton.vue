2136bbf5badac16da79941dedb32d460010c688d
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-05-08 20:44:45 +0300

    enhancement: add some colors to the checkbox button

--- src/components/checkbox/CheckboxButton.vue
+++ src/components/checkbox/CheckboxButton.vue
@@ -25,4 +25,16 @@ export default {
 <style lang="scss">
 @import "../../styles/theme";
 
+.el-checkbox-button {
+  color: $dark-body-grey;
+}
+.el-checkbox-button.is-focus {
+  border-color: $light-body-grey;
+}
+.el-checkbox-button.is-checked .el-checkbox-button__inner {
+    background-color: $primary-fill;
+    color: $primary-color;
+    border-color: $primary-fill;
+    -webkit-box-shadow: -1px 0 0 0 #8585ff;
+}
 </style>
