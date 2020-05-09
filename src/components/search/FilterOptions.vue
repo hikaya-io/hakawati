fba34323ce9a4056e04877982b4aa65a53df5bd0
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-05-09 12:52:00 +0300

    Search bar and filter (#86)
    * clean alert, breadcrumb, card, cascader
    
    * clean checkbox and datepicker
    
    * clean dialog
    
    * wip: added autocomplete element
    
    used the autocomplete element as the base component for plain search
    
    * wip: style plain search component
    
    * enhancement: plain search component
    
    - added expanding animation when focused
    
    * wip: filter
    
    - added filter component
    - used popover as base component
    - added some styling
    
    * enhancement: added border radius to filter
    
    * styling and css clean up to form
    
    * cleaned input, nav, pagination, select components
    
    * Avatar (#85)
    
    * add avatar
    
    * removed scoped
    
    * cleaned up remaining component styling
    
    * addressed comments
    
    * Label input (Issue 65) (#69)
    
    * Label input added with disabled and required property
    
    * component naming changed
    
    * improved: ui changes
    
    * fix: imported component in index file and model
    
    * optimized label input
    
    * fixed v model issue
    
    * fixed v model binding
    
    Co-authored-by: Tanvir Rahman <pathaoltd.@Tanvir-3.local>
    Co-authored-by: Tanvir Rahman <rahmantanvir@gmail.com>
    
    * fix: checkbox button component render issue
    
    * enhancement: add some colors to the checkbox button
    
    * add sytling to checkbox button
    
    * Drawer styling: Adjust height, shadow, speed (#68)
    
    * style: Add shadow and change drawer height
    
    * style: Increase time for drawer in/out animation
    
    * update double drawer style
    
    Co-authored-by: Andrew <andrew.tc.pham@gmail.com>
    
    * styling changes to search and filter boxes
    
    * wip: added autocomplete element
    
    used the autocomplete element as the base component for plain search
    
    * wip: style plain search component
    
    * enhancement: plain search component
    
    - added expanding animation when focused
    
    * wip: filter
    
    - added filter component
    - used popover as base component
    - added some styling
    
    * enhancement: added border radius to filter
    
    * styling changes to search and filter boxes
    
    * enhancement: styling and cosmetics changes
    
    - added btn-pill class to global css
    
    Co-authored-by: andrewtpham <andrew.tc.pham@gmail.com>
    Co-authored-by: Tanvir Rahman <rahmantanvir25558@gmail.com>
    Co-authored-by: Tanvir Rahman <pathaoltd.@Tanvir-3.local>
    Co-authored-by: Tanvir Rahman <rahmantanvir@gmail.com>
    Co-authored-by: Ninette Adhikari <13760198+ninetteadhikari@users.noreply.github.com>

---
+++ src/components/search/FilterOptions.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <el-popover placement="bottom" width="600" trigger="manual" v-model="visible" :visible-arrow=false >
+      <el-row :gutter="20"  class="align-center">
+        <el-col :span="3" class="spacer">
+          <div>
+            <i class="el-icon-s-operation"></i>
+          </div>
+        </el-col>
+
+        <el-col :span="6">
+          <el-row :gutter="20">
+            <el-col class="spacer">
+              <b>Last Login</b>
+            </el-col>
+
+            <el-col class="spacer">
+                  <checkbox-button text="None" class="button-pill"/>
+            </el-col>
+
+            <el-col class="spacer">
+             <checkbox-button text="Newest" class="button-pill"/>
+            </el-col>
+
+             <el-col class="spacer">
+            <checkbox-button text="Oldest" class="button-pill"/>
+            </el-col>
+          </el-row>
+        </el-col>
+
+        <el-col :span="6">
+          <el-row :gutter="20">
+            <el-col class="spacer">
+              <b>Role</b>
+            </el-col>
+
+              <el-col class="spacer">
+            <checkbox-button text="None" class="button-pill"/>
+            </el-col>
+
+            <el-col class="spacer">
+             <checkbox-button text="Member" class="button-pill"/>
+            </el-col>
+
+             <el-col class="spacer">
+             <checkbox-button text="Admin" class="button-pill"/>
+            </el-col>
+
+            <el-col class="spacer">
+           <checkbox-button text="View" class="button-pill"/>
+            </el-col>
+          </el-row>
+        </el-col>
+
+        <el-col :span="6">
+          <el-row :gutter="20">
+            <el-col class="spacer">
+              <b>Status</b>
+            </el-col>
+
+               <el-col class="spacer">
+             <checkbox-button text="None" class="button-pill"/>
+            </el-col>
+
+            <el-col class="spacer">
+            <checkbox-button text="Invited" class="button-pill"/>
+            </el-col>
+
+             <el-col class="spacer">
+            <checkbox-button text="Active" class="button-pill"/>
+            </el-col>
+
+            <el-col class="spacer">
+             <checkbox-button text="Inactive" class="button-pill"/>
+            </el-col>
+          </el-row>
+        </el-col>
+
+        <el-col :span="3">
+          <el-row :gutter="20">
+            <el-col class="spacer">
+              <b>
+                <i class="el-icon-close" @click="visible = !visible"></i>
+              </b>
+            </el-col>
+          </el-row>
+          <div></div>
+        </el-col>
+      </el-row>
+      <el-button slot="reference" type="text" @click="visible = !visible">
+        <i class="el-icon-s-operation"></i> Filter
+      </el-button>
+    </el-popover>
+  </div>
+</template>
+
+<script>
+import CheckboxButton from '../checkbox/CheckboxButton.vue'
+import TextButton from "../button/TextButton"
+
+export default {
+  components: { CheckboxButton, TextButton },
+
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../styles/theme";
+
+.spacer {
+  padding: 15px 33px;
+}
+
+.el-popover {
+  border-radius: 25px;
+  color: $dark-body-grey;
+}
+
+.align-center {
+  text-align: center;
+}
+
+// Removes checkbox button border
+.el-checkbox-button:first-child .el-checkbox-button__inner {
+    border-left: transparent;
+}
+
+.el-checkbox-button.is-checked .el-checkbox-button__inner {
+    color: $primary-color;
+    background-color: $primary-fill;
+    border-color: $primary-fill;
+}
+
+.el-checkbox-button.is-focus .el-checkbox-button__inner {
+    border-color: transparent;
+}
+</style>
