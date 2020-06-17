7602bf51d60ce8e14402ff4f7f33899b74eab1fb
Michael Bukachi <michaelbukachi@gmail.com>
2020-06-17 21:55:21 +0300

    feat: Add grid component

---
+++ src/components/grid/ComplexItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <h-card>
+    <div slot="title" class="body-reg">
+      <span>{{ title }}</span>
+    </div>
+    <div slot="items">
+      <p>{{ description }}</p>
+      <h-button type="primary" @click="$emit('button-clicked')">Click</h-button>
+    </div>
+  </h-card>
+</template>
+<script>
+import HCard from '../card/HCard'
+import HButton from '../button/HButton'
+
+export default {
+  name: 'ComplexItem',
+  components: {
+    HButton,
+    HCard
+  },
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
+<style lang="scss">
+  .card {
+    height: 100%;
+  }
+</style>
---
+++ src/components/grid/Item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Item',
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
