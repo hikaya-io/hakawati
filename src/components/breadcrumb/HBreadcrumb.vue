6f095829bb0309dee55b7cd1fd645c5818c34aae
andrewtpham <andrew.tc.pham@gmail.com>
2020-07-03 12:20:56 +0200

    adjust breadcrumb height to align

--- src/components/breadcrumb/HBreadcrumb.vue
+++ src/components/breadcrumb/HBreadcrumb.vue
@@ -39,12 +39,14 @@ export default {
       opacity: 0.3;
       padding: 50px 31px 67px 0px;
       font-size: 36px;
+      line-height: 43px;
       font-weight: bold;
   }
 
   .el-breadcrumb__item:last-child .el-breadcrumb__inner, .el-breadcrumb__item:last-child .el-breadcrumb__inner:hover, .el-breadcrumb__item:last-child .el-breadcrumb__inner a, .el-breadcrumb__item:last-child .el-breadcrumb__inner a:hover {
       color: $dark-body-grey;
       font-size: 36px;
+      line-height: 43px;
       font-weight: bold;
       padding: 50px 31px 67px 0px;
   }
