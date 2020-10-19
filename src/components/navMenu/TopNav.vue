7f94c523d96f1d4991806650aede4799237ba071
Michael Bukachi <michaelbukachi@gmail.com>
2020-10-20 01:59:40 +0300

    feat: Add responsiveness to top navbar

--- src/components/navMenu/TopNav.vue
+++ src/components/navMenu/TopNav.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="top-nav">
-    <el-menu
-      :default-active="activeIndex"
-      mode="horizontal"
-      v-bind="$attrs"
-      v-on="$listeners"
-    >
-    <slot></slot>
-    </el-menu>
+    <div id="menu" :class="{ active: isActive }">
+      <el-menu
+        :default-active="activeIndex"
+        mode="horizontal"
+        v-bind="$attrs"
+        v-on="$listeners"
+      >
+        <slot></slot>
+      </el-menu>
+    </div>
+    <div id="toggle" @click="isActive = !isActive">
+      <div class="span" id="top" :class="{ active: isActive }"></div>
+      <div class="span" id="middle" :class="{ active: isActive }"></div>
+      <div class="span" id="bottom" :class="{ active: isActive }"></div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +25,11 @@ export default {
     activeIndex: {
       type: String,
       default: ''
+    }
+  },
+  data () {
+    return {
+      isActive: false
     }
   }
 }
@@ -122,4 +134,83 @@ export default {
     background-color: $primary-fill;
 }
 
+</style>
+<style lang="scss">
+@import "../../styles/theme";
+@import "../../styles/variables";
+
+.top-nav {
+  width: 100%;
+  position: fixed;
+  top: 0;
+}
+
+#toggle {
+  position: absolute;
+  right: 20px;
+  top: 14px;
+  z-index: 999;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  float: right;
+  transition: all .3s ease-out;
+  visibility: hidden;
+  opacity: 0;
+}
+
+#toggle .span {
+  border-radius: 10px;
+  background: $primary-color;
+  transition: all 0.3s ease-out;
+  backface-visibility: hidden;
+}
+
+#top.span.active {
+  transform: rotate(45deg) translateX(3px) translateY(5px);
+}
+
+#middle.span.active {
+  opacity: 0;
+}
+
+#bottom.span.active {
+  transform: rotate(-45deg) translateX(8px) translateY(-10px);
+}
+
+@media only screen and (max-width: $screen-md-min) {
+
+  .logo-header {
+    padding: 0;
+  }
+
+  #toggle {
+    visibility: visible;
+    opacity: 1;
+    margin-top: 6px;
+  }
+
+  #toggle .span {
+    height: 4px;
+    margin: 5px 0;
+    transition: all .3s ease-out;
+    backface-visibility: visible;
+    visibility: visible;
+    opacity: 1;
+  }
+
+  #menu .el-menu-item,
+  #menu .el-submenu {
+    display: none;
+  }
+
+  #menu.active {
+    margin: 70px 0;
+    visibility: visible;
+    opacity: 0.98;
+    transition: all .5s ease-out;
+
+    @include sm-menu;
+  }
+}
 </style>
