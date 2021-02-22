a63d35f5f09e4b904157a937efc6d9adc0faab20
andrewtpham <andrew.tc.pham@gmail.com>
2021-02-22 17:08:46 +0100

    update h-card

--- src/components/card/HCard.vue
+++ src/components/card/HCard.vue
@@ -34,7 +34,7 @@ export default {
   .el-card.is-always-shadow {
     background: $white;
     box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
+    border-radius: 20px;
     border-color: transparent;
   }
 
