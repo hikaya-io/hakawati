3eb8722e6d5b1e392d7e3eddf7c32359f8e99eba
Ninette Adhikari <ninette.adhikari@gmail.com>
2022-05-27 13:15:09 +0200

    feat: Add color picker component

---
+++ src/components/colorPicker/HColorPicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-color-picker
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+  </el-color-picker>
+</template>
+
+<script>
+export default {
+  name: 'HColorPicker'
+}
+</script>
+
+<style lang="scss">
+@import "../../styles/theme";
+
+.el-button.el-color-dropdown__link-btn {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.el-button.el-color-dropdown__btn {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  border: 1px solid $light-body-grey;
+}
+.el-color-dropdown__link-btn {
+  padding: 0px;
+}
+</style>
