e14a62a4b84a59d4664fec63a317947d8476899a
Ninette Adhikari <13760198+ninetteadhikari@users.noreply.github.com>
2022-06-09 11:35:48 +0200

    Add custom color (#315)
    * fix(h-tag): Add custom color
    
    * build: Upgrade version to 89
    
    * fix(h-tag): Add agrs for type, size
    
    * fix(h-tag): Add props for type, size
    
    * fix(h-tag): Move story to 1.0
    
    * fix(h-tag): Comment out dynamic tag

--- src/components/tag/HTag.vue
+++ src/components/tag/HTag.vue
@@ -1,23 +1,73 @@
 <template>
-  <div :class="tagClass">
   <el-tag
     v-bind="$attrs"
     v-on="$listeners"
+    :custom-color="customColor"
+    :color="lightBackgroundColor"
+    :type="type"
+    :size="size"
+    :closable="closable"
+    :class="tagClass"
   >
-    <slot></slot>
+  <slot></slot>
   </el-tag>
-  </div>
 </template>
 
 <script>
 export default {
   name: 'HTag',
   props: {
-    plain: Boolean
+    type: String,
+    size: String,
+    plain: Boolean,
+    closable: Boolean,
+    customColor: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted () {
+    this.updateTextColor()
   },
   computed: {
     tagClass () {
       return !this.plain ? { 'h-tag': true } : null
+    },
+    lightBackgroundColor () {
+      if (this.customColor) {
+        this.updateTextColor()
+        return this.lightenColor(this.customColor, 90)
+      }
+      return null
+    }
+  },
+  methods: {
+    lightenColor (color, percent) {
+      // Reference: https://gist.github.com/renancouto/4675192
+      const num = parseInt(color.replace('#', ''), 16)
+      const amt = Math.round(2.55 * percent)
+      const R = (num >> 16) + amt
+      const B = (num >> 8 & 0x00FF) + amt
+      const G = (num & 0x0000FF) + amt
+      return (
+        '#' +
+        (
+          0x1000000 +
+          (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+          (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
+          (G < 255 ? (G < 1 ? 0 : G) : 255)
+        )
+          .toString(16)
+          .slice(1)
+      )
+    },
+    updateTextColor () {
+      const elTagArray = document.getElementsByClassName('el-tag')
+      for (let i = 0; i < elTagArray.length; i++) {
+        if (elTagArray[i].attributes['custom-color'].nodeValue) {
+          elTagArray[i].style.color = this.customColor
+        }
+      }
     }
   }
 }
@@ -27,26 +77,27 @@ export default {
 @import "../../styles/theme";
 
 .h-tag {
-  .el-tag {
+  &.el-tag {
     border-radius: 25px;
     border-color: transparent;
     text-align: center;
     padding: 0px 24px;
-  }
-  .el-tag.el-tag--success {
-    border-color: transparent;
-  }
 
-  .el-tag.el-tag--info {
-    border-color: transparent;
+    &.el-tag--success {
+      border-color: transparent;
+    }
+    &.el-tag--info {
+      border-color: transparent;
+    }
+    &.el-tag--warning {
+      border-color: transparent;
+    }
+    &.el-tag--danger {
+      border-color: transparent;
+    }
   }
-
-  .el-tag.el-tag--warning {
-    border-color: transparent;
-  }
-
-  .el-tag.el-tag--danger {
-    border-color: transparent;
-  }
+}
+.el-tag .el-tag__close:hover {
+  background-color: transparent;
 }
 </style>
--- src/stories/Tag.stories.js
+++ src/stories/Tag.stories.js
@@ -1,56 +1,46 @@
 import HTag from '../components/tag/HTag.vue'
-import DynamicTag from '../components/tag/DynamicTag.vue'
+// import DynamicTag from '../components/tag/DynamicTag.vue'
 
 // This is required for each story
-export default { title: 'Tag' }
-
-// Customize components here
-export const hTag = () => ({
-  components: { HTag },
-  template: `
-    <div>
-    <h-tag>Agriculture</h-tag>
-    <p />
-    <h-tag type="success">Education</h-tag>
-    <p />
-    <h-tag type="info">WASH</h-tag>
-    <p />
-    <h-tag type="warning">Health</h-tag>
-    <p />
-    <h-tag type="danger">Resilience</h-tag>
-    <p />
-
-    </div>
-  `
-})
-
-export const hTagPlain = () => ({
-  components: { HTag },
-  template: `
-    <div>
-    <h-tag plain size="mini">Beta</h-tag>
-    <p />
-    <h-tag plain size="small">Beta</h-tag>
-    <p />
-    <h-tag plain size="medium">Beta</h-tag>
-    <p />
-    <h-tag plain>Beta</h-tag>
-    </div>
-  `
-})
-
-const dynamicTags = ['Tag 1', 'Tag 2', 'Tag 3']
-
-export const dynamicTag = () => ({
-  components: { DynamicTag },
-  data () {
-    return {
-      dynamicTags
+export default {
+  component: HTag,
+  title: '1.0/Tag',
+  argTypes: {
+    type: {
+      options: ['primary', 'success', 'info', 'warning', 'danger'],
+      control: { type: 'select' }
+    },
+    size: {
+      options: ['large', 'medium', 'small', 'mini'],
+      control: { type: 'select' }
+    },
+    customColor: {
+      control: 'color'
     }
-  },
-  template: `
-      <dynamic-tag
-        :dynamicTags="dynamicTags"
-      />
-      `
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { HTag },
+  props: Object.keys(argTypes),
+  template: '<h-tag v-bind="$props">Custom</h-tag>'
 })
+
+export const Main = Template.bind({})
+Main.args = { }
+
+// const dynamicTags = ['Tag 1', 'Tag 2', 'Tag 3']
+//
+// export const dynamicTag = () => ({
+//   components: { DynamicTag },
+//   data () {
+//     return {
+//       dynamicTags
+//     }
+//   },
+//   template: `
+//       <dynamic-tag
+//         :dynamicTags="dynamicTags"
+//       />
+//       `
+// })
